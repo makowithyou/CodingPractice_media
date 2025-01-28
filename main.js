6fe8e5f56a22f5765bdde86b7d5b55fd93310fe9
makowithyou <mako.withyou@gmail.com>
2025-01-28 11:24:45 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-import {HamburgerEvent} from './hamburgerEvent.js';
+import {HamburgerEvent} from './module/hamburgerEvent.js';
+import {HamburgerEvent} from './module/scrollByEvent.js';
+
+
 
 const HamburgerButton = document.getElementById("HamburgerButton");
 HamburgerButton.addEventListener("click", HamburgerEvent, false);
+
+addEventListener("click", ScrollByEvent, false);
