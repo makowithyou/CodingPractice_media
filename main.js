ba037bbb3d268c97ac0d75b66cc22b0cac77eed8
makowithyou <mako.withyou@gmail.com>
2025-01-28 10:33:12 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+import {HamburgerEvent} from './hamburgerEvent.js';
+
+const HamburgerButton = document.getElementById("HamburgerButton");
+HamburgerButton.addEventListener("click", HamburgerEvent, false);
