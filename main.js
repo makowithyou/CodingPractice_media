6b136d25841ac38e332518f76d625d9f032170e1
makowithyou <mako.withyou@gmail.com>
2025-02-03 08:24:34 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,12 @@
 import {HamburgerEvent} from './module/hamburgerEvent.js';
 import {ScrollByEvent} from './module/scrollByEvent.js';
 import {FadeInEvent} from './module/fadeInEvent.js';
+import {SliderEvent} from './module/sliderEvent.js';
+
+SliderEvent();
 
 const HamburgerButton = document.getElementById("HamburgerButton");
 HamburgerButton.addEventListener("click", HamburgerEvent, false);
 
 addEventListener("click", ScrollByEvent, false);
 addEventListener("scroll", FadeInEvent, false);
-
-
-$(document).ready(function(){
-  $('#Slider').slick({
-     infinite: true,
-     arrows: false,
-     centerMode: true,
-     centerPadding: "13.3%",
-     mobileFirst: true,
-     responsive: [{
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 3,
-        centerPadding: "6.25%",
-      }
-    }]
-  });
-});
