845d1e68b7b279211751d9950d2de1ed00e5f0cc
makowithyou <mako.withyou@gmail.com>
2025-02-03 07:09:17 +0400

    Update main.js

--- main.js
+++ main.js
@@ -9,13 +9,19 @@ addEventListener("click", ScrollByEvent, false);
 addEventListener("scroll", FadeInEvent, false);
 
 
-// const Slider = document.getElementById("Slider");
-// for (let i = 1; i <= 9; i++) {
-//     const Img = document.createElement("img");
-//     const BaseSrcText = `https://github.com/makowithyou/CodingPractice_media/blob/main/imges/feature${i}.jpg?raw=true`;
-//     const BaseAltText = `特集${i}の画像`;
-
-//     Img.setAttribute("src", BaseSrcText);
-//     Img.setAttribute("alt", BaseAltText);
-//     Slider.appendChild(Img);
-// }
+$(document).ready(function(){
+  $('#Slider').slick({
+     infinite: true,
+     arrows: false,
+     centerMode: true,
+     centerPadding: "13.3%",
+     mobileFirst: true,
+     responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        centerPadding: "6.25%",
+      }
+    }]
+  });
+});
