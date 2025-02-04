5e948b8750b834243a7c94ea5cb3029af3c2751b
makowithyou <mako.withyou@gmail.com>
2025-02-04 13:27:36 +0900

    Update fadeInEvent.js

--- module/fadeInEvent.js
+++ module/fadeInEvent.js
@@ -1,16 +1,17 @@
 const InnerHeight = window.innerHeight;
 const ImgFade = document.getElementsByClassName("ImgFade");
 const ImgHeight = ImgFade[0].height;
-
 function FadeInEvent() {
-    for (const img of ImgFade){
-        const ImgBottom = img.y + ImgHeight;
-        if (ImgBottom < InnerHeight){
-            if (!img.classList.contains("inview")) {
-                img.classList.add("inview");
+    window.addEventListener("scroll", ()=>{
+        for (const img of ImgFade){
+            const ImgBottom = img.y + ImgHeight;
+            if (ImgBottom < InnerHeight){
+                if (!img.classList.contains("inview")) {
+                    img.classList.add("inview");
+                }
             }
         }
-    }
+    });
 }
 
 export {FadeInEvent};
