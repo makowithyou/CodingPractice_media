a3ac66623ac80a72f96a8292a5352e734cf609b0
makowithyou <mako.withyou@gmail.com>
2025-02-06 12:10:08 +0900

    Update scrollByEvent.js

--- module/scrollByEvent.js
+++ module/scrollByEvent.js
@@ -1,8 +1,6 @@
 import {HamburgerEvent} from './hamburgerEvent.js';
 
 function ScrollByEvent(e) {
-    console.log("クリックされました");
-    console.log(e.target);
     const classList = e.target.classList;
     if (classList[0] == "scrollTo"){
         const targetId = classList[1];
